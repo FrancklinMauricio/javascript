bc2222c42d3d31f2aae247f01c7981a0246228e9
Francklin Mauricio <93860268+FrancklinMauricio@users.noreply.github.com>
2021-12-07 03:59:16 -0300

    Vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+var num = [1, 3, 5, 7, 9]
+num.push(2)
+console.log(num)
+console.log(`A quantidade de valores escolhidos são ${num.length}`)
+console.log(`O primeiro valor é ${num[0]}`)
+console.log(num.sort())
+
